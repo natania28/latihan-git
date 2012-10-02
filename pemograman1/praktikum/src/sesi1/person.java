192e461c12fb90e140eb8eb53766228570999955
unknown <User_38@.(none)>
2012-10-02 11:20:49 +0700

    natania

---
+++ pemograman1/praktikum/src/sesi1/person.java
@@ -0,0 +1,14 @@
+//Nama : Natania F.N
+//NIM  : 4511210020
+//Praktikum Pemograman 4 - 1
+
+public class person {
+public static void main(String[] args) {
+String nim = "4511210020";
+String nama;
+nama = "Natania";
+System.out.println("Nama saya adalah:" + nama);
+System.out.println("Nim: " + nim);
+}
+}
+
